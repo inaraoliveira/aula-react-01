f745b75c5ee2b74d5bfcd540c956f3fd4a42134d
Inara Oliveira <inaraolliveira23@hotmail.com>
2021-10-19 17:04:46 -0300

    Cria a primeira aplicação React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import coruja from './Coruja-buraqueira.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aprendendo React</h1>
+      <img alt='Coruja' src={coruja}></img>
+
+      <pre>
+      A coruja é simbolo da sabedoria por influência da mitologia grega, tanto que Atena, deusa da guerra
+      e da sabedoria, tinha uma coruja como mascote. 
+      </pre>
     </div>
   );
 }
